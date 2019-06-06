4e22bcce83b5c8590e2f1f987925054ff26176ce
ZB Tyoma <34129397+zb-venom@users.noreply.github.com>
2019-06-06 21:48:34 +0700

    Update index.php

--- index.php
+++ index.php
@@ -81,7 +81,7 @@ if (isset($_POST['new_post']))
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <a class="navbar-brand" href="index.php">ШКАФ</a>
+            <a class="navbar-brand" href="index.php">ADAS</a>
 
             <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
                 <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
